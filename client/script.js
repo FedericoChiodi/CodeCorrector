294ff4908357d35c0112c4dc471ee12974f976b7
FedericoChiodi <federico.chiodi@edu.unife.it>
2023-09-16 13:14:56 +0200

    Ported to new API

--- client/script.js
+++ client/script.js
@@ -4,6 +4,13 @@ const form = document.querySelector('form');
 const responseArea = document.getElementById('responseArea');
 const button = document.getElementById('submitButton');
 
+/* 
+* Questa funzione si occupa di scrivere la risposta ricevuta contenente il risultato
+* dell'elaborazione da parte del modello in stile chat, quindi scrivendo un carattere
+* per volta seguito da un breve delay.
+* 'element' contiene l'elemento dove sarà scritto il testo.
+* 'text' contiene il testo da scrivere.
+*/
 function typeText(element, text) {
   let index = 0
 
@@ -18,6 +25,10 @@ function typeText(element, text) {
   }, 10)
 }
 
+/*
+* Questa funzione gestisce le azioni da compiere quando viene inviato il form che l'utente
+* ha riempito con il codice da correggere.
+*/
 const handleSubmit = async (e) => {
   e.preventDefault()
 
@@ -26,6 +37,11 @@ const handleSubmit = async (e) => {
   button.disabled = true
   let buttonText = button.innerHTML
 
+  /*
+  * Se il prompt non è vuoto viene impostato un messaggio di attesa nella textarea dedicata
+  * a contenere la risposta del modello e si invia al server, con metodo POST, un oggetto
+  * JSON contenente il prompt dell'utente codificato.
+  */
   if (data.get('prompt') !== ""){
     button.innerHTML = ""
     responseArea.innerHTML = "CodeCorrector sta pensando ..."
@@ -38,7 +54,10 @@ const handleSubmit = async (e) => {
         prompt: data.get('prompt')
       })
     })
-  
+    /*
+    * Se la risposta del server contiene uno status code positivo
+    * la inizio a scrivere a schermo, altrimenti stampo un messaggio di errore.
+    */
     if (response.ok) {
       const data = await response.json();
       const parsedData = data.bot.trim()
--- server/server.js
+++ server/server.js
@@ -1,45 +1,61 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
-
-// Configurazioni
+import OpenAI from 'openai';
+/*
+* Necessaria per caricare le variabili d'ambiente.
+* Caricherà la APIkey di OpenAI salvata in una variabile d'ambiente per
+* motivi di sicurezza.
+*/
 dotenv.config()
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
-// Istanza di openaiAPI
-const openai = new OpenAIApi(configuration);
-
-// Inizializzazione middleware
+/*
+* Preparo la configurazione da usare per le API e
+* inizializzo Express
+*/
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Gestione delle richieste GET
+/*
+* Le richieste GET fatte alla radice sono gestite
+* con un semplice messaggio di conferma, utile
+* per il debug e per verificare che il server sia operativo.
+*/
 app.get('/', async (req, res) => {
     res.status(200).send({
       message: 'Response OK'
     })
 })
 
-// API call ad OpenAI, modello che completa codice. Prompt prefabbricato
+/*
+* Le richieste POST vengono gestite estraendo dal corpo della richiesta
+* il prompt dell'utente passato in JSON e inviando una API request ad OpenAI.
+* La chiamata alle API è formata da un prompt prefabbricato contenente quello dell'utente 
+* e da altri campi necessari ad impostare alcuni parametri del modello.
+* La risposta ottenuta dal modello viene poi inviata al client.
+*/
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model: "code-davinci-002",
-      prompt: `//*// Fix and explain bugs in this code:\n${prompt}\n// Fixed code:`,
-      temperature: 0.1,
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages:[
+        {"role": "system", "content": "You are a master programmer with over thirty years of experience."},
+        {"role": "user", "content": "The code I am writing contains bugs and does not work properly. Can you fix my code and explain all bugs you correct? Please be concise in your response and only type the fixed code followed by the explanation. Do not use ``` code blocks in your response, type the code as normal text."},
+        {"role": "assistant", "content": "Of course, please send me your code so I can help you."},
+        {"role": "user", "content": prompt},
+      ],
+      temperature: 0.05,
       max_tokens: 1024,
-      stop: ["//*//"],
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response.choices[0].message.content
     });
   }
   catch (error) {
@@ -48,5 +64,7 @@ app.post('/', async (req, res) => {
   }
 })
 
-// Esposizione del server sulla porta 5000
+/*
+* Il server viene aperto sulla porta 5000
+*/
 app.listen(5000, () => console.log('Server started successfully on port 5000'))
